a708534c3d6fefe7c2a82ad65b841749ec7f79cb
thekill022 <gibfathoni@gmail.com>
2026-03-14 13:13:56 +0800

    set static color variable

--- lib/mainlayout.dart
+++ lib/mainlayout.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MainLayout extends StatelessWidget {
+  static const Color primaryColor = Color(0xFF4D51ED);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color accentYellow = Color(0xFFFFC107);
+  static const Color accentOrange = Color(0xFFFF7043);
+
+  static const Color textTitleColor = Color(0xFF2D2D2D);
+  static const Color textSubtitleColor = Color(0xFF666666);
+  static const Color inputFillColor = Color(0xFFF8F9FA);
+  static const Color inputBorderColor = Color(0xFFE0E0E0);
+  static const Color labelColor = Color(0xFF4A4A44);
+
   const MainLayout({super.key});
 
   @override
