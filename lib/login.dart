43942a35a34791f071897fd9005da9308679b74c
thekill022 <gibfathoni@gmail.com>
2026-03-14 13:02:45 +0800

    membuat state login dan inisialisasi variable

---
+++ lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
