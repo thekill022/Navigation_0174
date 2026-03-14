8cbccc18de2cb851fb50093f17aeaab327b9e842
thekill022 <gibfathoni@gmail.com>
2026-03-14 21:46:16 +0800

    inisialisasi variable yang diperlukan untuk controller, total harga, dan formkey

---
+++ lib/order.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
